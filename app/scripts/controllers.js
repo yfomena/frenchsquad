4ee2fe8fb9d5eeb764793362a9553384ecfa1d12
FOMENA Yannick <Yannick.FOMENA@murex.com>
2017-01-13 19:22:21 +0100

    adding trait description

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -80,52 +80,52 @@ angular.module('frenchsquadApp')
 								{
 								  title:'Chance',
 								  image: 'images/luck.jpg',
-								  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+								  description:'Augmente les chances que d’autres traits prennent effet de X % (10% argent, 15% or). Le calcul ne consiste pas à ajouter ce pourcentage à celui des autres traits, il s\' agit plutôt un coefficient multiplicateur.<br> <b>Exemple:</b> Si vous avez une arme avec 80% de chance d\'interrompre un ennemi et 10% de chance, votre trait interruption passe de 80 à <i>80 + 80*10%</i> soit 88% '
 								},
 								{
                   title:'Destruction',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Augmente les dégâts de X % Uniquement dans les cas où vous faites un coup critique: 40% si le trait est argent et 60% si le trait est or'
                 },
 								{
                   title:'Intercepteur',
                   image: 'images/luck.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Les attaques en vigilance qui ne sont pas des tirs au corps ont X % de chance d\'interrompre l\'ennemi.<br> Attention, au-delà du niveau de votre personnage + 3 (<b>exemple:</b> pour un niveau 20, à partir des ennemis de niveau 24) vous ferez systématiquement des tirs au corps. Chaque étoile rose que gagne votre personnage incrémente de 1 le niveau de tirs au corps (<b>Exemple:</b> Un personnage niveau 20 avec une étoile rose fera systématiquement des tirs au corps sur des ennemis de niveau 25) '
                 },
 								{
                   title:'Specialiste des armes',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'les armes infligent X %(10% argent, 15% or) de dégâts supplémentaires'
                 },
 								{
                   title:'Precision',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Augmente les chances de coup critique de X % (10% argent et 15% or)'
                 },
 								{
                   title:'Commotion',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Ce trait n\'est présent que pour des armes de cogneur. Vos attaques étourdissantes ont X %(40% argent, 60% or) de chances d’immobiliser l’ennemi pendant un tour supplémentaire. <br> <b>Attention:</b> Comme le trait <b>intercepteur</b>, au delà du niveau de votre personnage +3, l\'ennemi ne sera pas immobilisé.'
                 },
 								{
                   title:'Amplitude',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Ce trait ne s\'applique qu\'aux armes de guerrier. Il augmente l\'amplitude d\'attaque de X°(90° argent et 130° or), vous permettant ainsi d\'atteindre plusieurs cibles en même temps.'
                 },
 								{
                   title:'Coup rapide',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Ce trait ne s\'applique qu\'aux armes d\'éclaireur. Il vous confère X %(30% argent, 40% or) de chance en plus d\'avoir une attaque gratuiteaprès avoir tué un ennemi.'
                 },
 								{
                   title:'Reduction de la menace',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Ce trait ne s\'applique qu\'aux armes d\'éclaireur. Il donne X %(50% argent, 75% or) de chances, après avoir tué un ennemi, de réduire de un le nombre d\'ennemis qui arriveront à la prochaine vague.'
                 },
 								{
                   title:'Charge',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Present uniquement sur des armes de cogneur et éclaireur (chez les corps à corps), ce trait vous donne X %(20% argent, 30% or) de chances, après avoir tué un ennemi, de gagner un point supplémentaire de charge critique.<br> L\'attaque critique en question fait X %(12% argent, 15% or) de dégats supplémentaires.'
                 }
               ]
         }])
@@ -135,57 +135,57 @@ angular.module('frenchsquadApp')
                 {
                   title:'Chance',
                   image: 'images/luck.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Augmente les chances que d’autres traits prennent effet de X % (10% argent, 15% or). Le calcul ne consiste pas à ajouter ce pourcentage à celui des autres traits, il s\' agit plutôt un coefficient multiplicateur.<br> <b>Exemple:</b> Si vous avez une arme avec 80% de chance d\'interrompre un ennemi et 10% de chance, votre trait interruption passe de 80 à <i>80 + 80*10%</i> soit 88% '
                 },
                 {
                   title:'Destruction',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Augmente les dégâts de X % Uniquement dans les cas où vous faites un coup critique: 40% si le trait est argent et 60% si le trait est or'
                 },
                 {
                   title:'Intercepteur',
                   image: 'images/luck.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Les attaques en vigilance qui ne sont pas des tirs au corps ont X % de chance d\'interrompre l\'ennemi.<br> Attention, au-delà du niveau de votre personnage + 3 (<b>exemple:</b> pour un niveau 20, à partir des ennemis de niveau 24) vous ferez systématiquement des tirs au corps. Chaque étoile rose que gagne votre personnage incrémente de 1 le niveau de tirs au corps (<b>Exemple:</b> Un personnage niveau 20 avec une étoile rose fera systématiquement des tirs au corps sur des ennemis de niveau 25) '
                 },
                 {
                   title:'Specialiste des armes',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'les armes infligent X %(10% argent, 15% or) de dégâts supplémentaires'
                 },
                 {
                   title:'Precision',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Augmente les chances de coup critique de X % (10% argent et 15% or)'
                 },
                 {
                   title:'Dispersion',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Présent uniquement sur des armes de soldats, il augmente le cône d\'attaque de X° (33° argent, 47° or)'
                 },
                 {
                   title:'Gros calibre',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Ce trait n\'est présent que sur des armes de chasseurs. Il augmente la zone de tir de X mètres(0.3 argent, 0.5 or), permettant ainsi d\'atteindre des ennemis non disposés sur une même ligne.'
                 },
                 {
                   title:'surpuissance',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Uniquement sur des armes de tireurs, ce trait augmente la portée de X cases(2 argent, 3 or). La portée en cas de trait or est supérieure à celle du chasseur, ce qui offre un réel confort.'
                 },
                 {
                   title:'Silencieux',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Confère X %(20% argent, 30% or) de chance de ne pas générer de menace. Cela vous évite d’attirer de nouveaux rodeurs pour la prochaine vague'
                 },
                 {
                   title:'Transperceur',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Réduit de X % les chances de tir au corps'
                 },
                 {
                   title:'Charge',
                   image: 'images/dmg.jpg',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                  description:'Present uniquement sur des armes de tireurs (chez les distants), ce trait vous donne X %(20% argent, 30% or) de chances, après avoir tué un ennemi, de gagner un point supplémentaire de charge critique.<br> L\'attaque critique en question fait X %(12% argent, 15% or) de dégats supplémentaires'
                 }
               ]
         }])
